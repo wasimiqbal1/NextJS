874e9abe2b874777e80cc64c15eaa3186e8b1ce0
unknown <wasim_iqbal@msn.com>
2023-02-15 01:16:50 +0500

    Navigate Between Pages

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -11,9 +12,12 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+        <h1 className="title">
+          go to <Link href="/posts/first-post">this page!</Link>
+        </h1>
       </section>
     </Layout>
   );
