c5303de3e71ad02007cf414d3c1abc19a801da2f
unknown <wasim_iqbal@msn.com>
2023-02-18 15:40:04 +0500

    NextJS Blog

---
+++ nextjs-blog/pages/posts/[id].js
@@ -0,0 +1,49 @@
+import Layout from '../../components/layout';
+import Head from 'next/head';
+import Date from '../../components/date';
+
+
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+export async function getStaticProps({ params }) {
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+ 
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      <Date dateString={postData.date} />
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
+}
+
+
+
+
+
