2ace4d3fcefe8aee4504860cc945e627fbf58504
unknown <wasim_iqbal@msn.com>
2023-02-19 18:03:17 +0500

    PricingUI Update

---
+++ pricingui/app/Header.tsx
@@ -0,0 +1,11 @@
+import { Box,  Heading } from '@chakra-ui/react'
+import React from 'react'
+
+export default function Header() {
+  return (
+  <Box textAlign="center" bg="#6B46C1" pt="90px" pb="200Px" color="white">
+    <Heading pb="10px">Simple pricing for your business</Heading>
+    <text>Plan that are carefully crafted to suit your business</text>
+  </Box>
+  )
+}
