f6976ab8e1021bed816d9c28d8aa4da710ceeca0
unknown <wasim_iqbal@msn.com>
2023-02-20 01:04:27 +0500

    PricingUi with Chakra

--- pricingui/app/Pricing.tsx
+++ pricingui/app/Pricing.tsx
@@ -26,22 +26,22 @@ export default function Pricing() {
         " 0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04);"
       }
     >
-      <Flex direction={{ base: "column", md: "column", lg: "row" }}>
-        <Box bg="#cbb8ee" p="60px" textAlign={"center"}>
+           <Flex direction={{ base: "column", md: "column", lg: "row" }}>
+           <Box bg="#cbb8ee" p="40px" textAlign={"center"}>
           <text fontSize={"24px"} fontWeight={"bold"}>
             Premium PRO
           </text>
           <Heading fontSize={"60px"}>$329</Heading>
           <text> Billed Just Once</text>
           <div>
-            <Button mt={"10px"} w={"300px"} color={"white"} bg={"#805ad5"}>
+            <Button mt={"20px"} w={"300px"} color={"white"} bg={"#805ad5"}>
               Get Started
             </Button>
           </div>
         </Box>
         <Box pt="50px" pl="25px" pr="10px">
           <Text mb="10px">
-            Access these futures when you get this pricing package for your
+                    Access these futures when you get this pricing package for your
             business.
           </Text>
           <HStack mb={"15px"}>
@@ -59,7 +59,7 @@ export default function Pricing() {
           <HStack mb={"15px"}>
             <Icon as={checkMarkIcon} />
             <Text>24/7 support and consulting</Text>
-          </HStack>{" "}
+          </HStack>
         </Box>
       </Flex>
     </Box>
